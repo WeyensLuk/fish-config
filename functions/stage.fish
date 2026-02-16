524d345225aecf8615402dce468863b0d3f95a47
Luk Weyens <lukweyens@gmail.com>
2026-02-16 10:34:28 +0100

    Update stage.fish
    Fix support for file paths with spaces/dashes/...

--- functions/stage.fish
+++ functions/stage.fish
@@ -1,3 +1,3 @@
 function stage
-  git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git add
+  git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {}' | xargs -I {} -r git add "{}"
 end
