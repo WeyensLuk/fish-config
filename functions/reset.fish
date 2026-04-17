5559d79b4e733d8ef0428bb466d5518b15180e11
Luk Weyens <lukweyens@gmail.com>
2026-04-17 08:50:24 +0200

    Update reset.fish
    Fix reset command

--- functions/reset.fish
+++ functions/reset.fish
@@ -1,3 +1,3 @@
 function reset
-    git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -r git reset
+    git ls-files --deleted --modified --other --exclude-standard | fzf -0 -m --preview 'git diff --color=always {-1}' | xargs -I {} -r git checkout -- "{}"
 end
